7f45c263e10d18a657059d2a1060b6f45983b879
Eleeey <ezralaakasen@gmail.com>
2024-05-09 17:01:36 +0100

    ......

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -2,8 +2,9 @@
 
 import { GripVertical } from "lucide-react"
 import * as ResizablePrimitive from "react-resizable-panels"
+import { cn } from "../../lib/utils"
 
-import { cn } from "/lib/utils"
+
 
 const ResizablePanelGroup = ({
   className,
