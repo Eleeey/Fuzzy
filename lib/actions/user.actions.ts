90e950fba8d66e3ee43726aa9154b131a43952c7
Eleeey <ezralaakasen@gmail.com>
2024-05-09 17:18:00 +0100

    ......

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,72 +1,72 @@
-"use server";
+// "use server";
 
-import { revalidatePath } from "next/cache";
-import { db } from "../db";
+// import { revalidatePath } from "next/cache";
+// import { db } from "../db";
 
 
 
-// CREATE
-export async function createUser(user: any) {
-  try {
+// // CREATE
+// export async function createUser(user: any) {
+//   try {
 
-    const newUser = await db.User.create({data:user});
+//     const newUser = await db.user.create({data:user});
 
-    return JSON.parse(JSON.stringify(newUser));
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     return JSON.parse(JSON.stringify(newUser));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-// READ
-export async function getUserById(userId: string) {
-  try {
+// // READ
+// export async function getUserById(userId: string) {
+//   try {
     
 
-    const user = await db.User.findOne({ clerkId: userId });
+//     const user = await db.user.findOne({ clerkId: userId });
 
-    if (!user) throw new Error("User not found");
+//     if (!user) throw new Error("User not found");
 
-    return JSON.parse(JSON.stringify(user));
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     return JSON.parse(JSON.stringify(user));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-// UPDATE
-export async function updateUser(clerkId: string, user: any) {
-  try {
+// // UPDATE
+// export async function updateUser(clerkId: string, user: any) {
+//   try {
     
-    const updatedUser = await db.User.findOneAndUpdate({ clerkId }, user, {
-      new: true,
-    });
+//     const updatedUser = await db.user.findOneAndUpdate({ clerkId }, user, {
+//       new: true,
+//     });
 
-    if (!updatedUser) throw new Error("User update failed");
+//     if (!updatedUser) throw new Error("User update failed");
     
-    return JSON.parse(JSON.stringify(updatedUser));
-  } catch (error) {
-    console.log(error);
-  }
-}
+//     return JSON.parse(JSON.stringify(updatedUser));
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-// DELETE
-export async function deleteUser(clerkId: string) {
-  try {
+// // DELETE
+// // export async function deleteUser(clerkId: string) {
+// //   try {
     
 
-    // Find user to delete
-    const userToDelete = await db.user.findOne({ clerkId });
+// //     // Find user to delete
+// //     const userToDelete = await db.user.findOne({ clerkId });
 
-    if (!userToDelete) {
-      throw new Error("User not found");
-    }
+// //     if (!userToDelete) {
+// //       throw new Error("User not found");
+// //     }
 
-    // Delete user
-    const deletedUser = await db.user.findByIdAndDelete(userToDelete._id);
-    revalidatePath("/");
+// //     // Delete user
+// //     const deletedUser = await db.user.findByIdAndDelete(userToDelete._id);
+// //     revalidatePath("/");
 
-    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-  } catch (error) {
-    console.log(error);
-  }
-}
+// //     return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+// //   } catch (error) {
+// //     console.log(error);
+// //   }
+// // }
 
